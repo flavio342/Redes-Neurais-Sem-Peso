5ac5864ecdcc5cab381513801003851ea0ef4c12
Flavio <flavioribeiro342@gmail.com>
2017-08-25 12:22:12 -0300

    show pa

---
+++ h.py
@@ -0,0 +1,89 @@
+import numpy
+import random
+
+hOne = [1,0,0,1,
+        1,1,1,1,
+        1,0,0,1,
+        1,0,0,1]
+
+hTwo = [1,0,0,1,
+        1,0,0,1,
+        1,1,1,1,
+        1,0,0,1]
+
+hThree = [1,0,0,1,
+          1,1,1,1,
+          1,1,1,1,
+          1,0,0,1]
+
+teste = [0,1,0,1,
+         0,1,0,0,
+         0,0,1,0,
+         1,0,1,0]
+
+class Rede:
+
+    class Neuronio:
+
+        def __init__(self,indicesDeEntradas):
+            self.tabelaDeDados = []
+            for i in range(2**len(indicesDeEntradas)):
+                self.tabelaDeDados.append(0)
+            self.indicesDeEntradas = indicesDeEntradas     
+
+    def __init__(self,nTotalDeEntradas,nEntradasPorNeuronio):
+        entradas = []
+        for i in range(nTotalDeEntradas):
+            entradas.append(i)
+        neuronios = []
+        for i in range(nTotalDeEntradas/nEntradasPorNeuronio):
+            indicesDeEntradasDoNeuronio = []
+            for j in range(nEntradasPorNeuronio):
+                r = random.choice(entradas)
+                entradas.remove(r)
+                indicesDeEntradasDoNeuronio.append(r)
+            neuronio = self.Neuronio(indicesDeEntradasDoNeuronio)
+            neuronios.append(neuronio)
+        self.neuronios = neuronios
+
+    def treinarRede(self, entrada):
+        for neuronio in self.neuronios:
+            coordenadaNaTabela = ""
+            for i in range(len(neuronio.indicesDeEntradas)):
+                coordenadaNaTabela += str(entrada[neuronio.indicesDeEntradas[i]])
+
+            indiceNaTabelaDeDados = int(coordenadaNaTabela, 2)
+            
+            neuronio.tabelaDeDados[indiceNaTabelaDeDados] += 1
+
+    def testeDeEntrada(self,entrada):
+
+        resultado = 0
+
+        for neuronio in self.neuronios:
+            coordenadaNaTabela = ""
+            for i in range(len(neuronio.indicesDeEntradas)):
+                coordenadaNaTabela += str(entrada[neuronio.indicesDeEntradas[i]])
+
+            indiceNaTabelaDeDados = int(coordenadaNaTabela, 2)
+            
+            resultado += neuronio.tabelaDeDados[indiceNaTabelaDeDados]
+        
+        print resultado
+        print "\nTire suas proprias conclusoes, humano.\n\n"
+
+
+
+
+rede = Rede(16,2)
+rede.treinarRede(hOne)
+rede.treinarRede(hTwo)
+rede.treinarRede(hThree)
+
+rede.testeDeEntrada(hOne)
+rede.testeDeEntrada(hTwo)
+rede.testeDeEntrada(hThree)
+rede.testeDeEntrada(teste)
+
+
+
